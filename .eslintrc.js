84eb781bcdf6b28505fe54a862d9fddfeded01f5
Krish Maha <krish@mahas.net>
2024-06-21 00:06:15 +0100

    feat: Set up routing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,22 @@
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
-    'airbnb',
   ],
   plugins: [
     'react',
