6f82f7b3e945610439a63a2d1163cf9b76ab73ed
Krish Maha <krish@mahas.net>
2024-06-22 02:32:46 +0100

    Format tags on home page

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -8,11 +8,15 @@ const Layout = () => {
     <div className='App'>
       <Sidebar />
       <div className='page'>
-        <span className='tags top-tags'>&lt;body&gt;</span>
+        <span className='top-tag-html'>
+        &lt;html&gt;
+        <br />
+        <span className='top-tag-indent'>&lt;body&gt;</span>
+        </span> 
 
         <Outlet />
 
-        <span className='tags bottom-tags'>
+        <span className='bottom-tag-indent'>
           &lt;/body&gt;
           <br />
           <span className='bottom-tag-html'>&lt;/html&gt;</span>
