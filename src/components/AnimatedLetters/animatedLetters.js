6ceaa09b6bb695222798d62b0204aa130726d16b
Krish Maha <krish@mahas.net>
2024-06-22 17:20:01 +0100

    Implement bounce in effect

---
+++ src/components/AnimatedLetters/animatedLetters.js
@@ -0,0 +1,26 @@
+import './animatedLetters.scss'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const AnimatedLetters = ({ letterClass, strArray, idx}) => {
+  return (
+    <span>
+      {
+        strArray.map((char, i) => (
+          <span key={char + i} className={`${letterClass} _${i + idx}`} style={{ display: 'inline-block' }}>
+            {char === ' ' ? '\u00A0' : char}
+          </span>
+        ))
+      }
+    </span>
+  )
+}
+
+AnimatedLetters.propTypes = {
+  letterClass: PropTypes.string.isRequired,
+  strArray: PropTypes.arrayOf(PropTypes.string).isRequired,
+  idx: PropTypes.number.isRequired,
+}
+
+export default AnimatedLetters;
+
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,15 +2,28 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
 import '../Layout/index.scss';
+import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters/animatedLetters';
 
 const Home = () => {
+
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const greetingArray = ['H', 'i', '!']
+  const nameArray = ['I', "'", 'm', ' ', 'K', 'r', 'i', 's', 'h']
+  const jobArray = ['a', ' ', 'f', 'u', 'l', 'l', '-', 's', 't', 'a', 'c', 'k', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+
+  console.log(setLetterClass)
+  console.log(jobArray)
   return (
     <div className='container home-page'>
       <div className='text-zone'>
-        <h1>Hi! <br /> I&apos;m Krish,
+        <h1>
+        <AnimatedLetters letterClass={letterClass} strArray={greetingArray} idx={12} />
+        <br />
+        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
         {/* <img src={LogoTitle} alt="developer" /> */}
         <br />
-        a full-stack developer
+        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={24} />
         </h1>
         <div className='subheading'>
           <h2>SWE Intern @ KAIKAKU AI</h2>
