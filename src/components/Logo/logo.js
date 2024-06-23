ed8a0f939b8ddf795ef0e79e1efc2e059d0d33ea
Krish Maha <krish@mahas.net>
2024-06-23 22:09:29 +0100

    Display moving cube on homepage

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -1,10 +1,13 @@
-import KrishLogo from '../../assets/images/krish-logo.png';
+
 import './logo.scss';
+import Cube from '../Cube/cube';
 
 const Logo = () => {
+  
   return (
-    <div className='logo-container'>
-      <img className='solid-logo' src={KrishLogo} alt='K' />
+    <div>
+      <Cube />
+      {/* <img className='logo-container' src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTRkeXdrNTAyZ2gwczg3NTZsa20xaHc1ZDdqZmlyZXJ6dmp6cHZzNSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/bGgsc5mWoryfgKBx1u/giphy.gif' alt='logo' /> */}
     </div>
   )
 }
